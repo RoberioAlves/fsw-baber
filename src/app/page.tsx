d2fda65d5d8087cdc5081bc97a8d7f7a4ec50dcb
RoberioAlves <roberiouol@gmail.com>
2024-08-08 19:46:00 -0300

    chore: Add shadc setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 "use client";
 
 import Image from "next/image";
+import { Button } from "./_components/ui/button";
 
 export default function Home() {
   return (
-    <h1 className="text-red-500">Hello Word!!!</h1>
+    <Button>acessar</Button>
   );
 }
