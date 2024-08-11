5acb27e4adfe7011c6b4d364bee64c15252efaad
RoberioAlves <roberiouol@gmail.com>
2024-08-10 21:19:15 -0300

    feat: add hearder component and banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,33 @@
 "use client"
 
-import Header from "./_components/ui/hearder"
+import { Input } from "./_components/ui/input"
+import Header from "./_components/hearder"
+import { Button } from "./_components/ui/button"
+import { SearchIcon } from "lucide-react"
+import Image from "next/image"
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Roberio</h2>
+        <p>Sexta-feira, 10 de agosto.</p>
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            alt="Agende nos melhores com FSW Barber"
+            src="/Barber-bro.png"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
     </div>
   )
 }
